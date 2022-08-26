1a9ee63aa54583b6342a0ce79d69405d52ad9e3f
apriliadiandraputri <112156746+apriliadiandraputri@users.noreply.github.com>
2022-08-26 14:01:33 +0700

    Add files via upload

---
+++ apriliadiandra12rpl1/profil.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <header>
+        <?php include 'nav.html';?>
+    </header>
+    <h1>Halaman Profil</h1>
+    <div class="card" style="width: 18rem;">
+    <img src="aprilia.png" class="card-img-top" alt="Foto Aprilia">
+    <div class="card-body">
+      <h5 class="card-title">Aprilia Diandra Putri</h5>
+      <p class="card-text">Saya Aprilia Diandra Putri dari kelas 12 Rpl 1. Nomor absen saya 06.</p>
+    </div>
+  </div>
+  </body>
+</html>
